8fcfaa14d5dac3c8149afed242251a9f90b75b4b
Artiom Tofan <artiomtofancv@gmail.com>
2020-05-16 14:54:25 +0200

    Added search function

--- routes/streamers.js
+++ routes/streamers.js
@@ -1,101 +1,124 @@
 //Adding dependancies
-const express = require("express"),
-    Streamer = require("../models/streamer"),
-    Comment = require("../models/comment"),
-    middleware = require("../middleware"),
-    router = express.Router();
+const express = require('express'),
+	Streamer = require('../models/streamer'),
+	Comment = require('../models/comment'),
+	middleware = require('../middleware'),
+	router = express.Router();
 
 //Rendering Streamers page
-router.get("/", (request, response) => {
-    //Getting all the streamers from Database
-    Streamer.find({}, (error, allStreamers) => {
-        if (error) {
-            console.log(error);
-        } else {
-            //Renders all the streamers and also contains the information of currently loged user
-            response.render("streamers/streamers", { streamers: allStreamers });
-        };
-    });
+router.get('/', (request, response) => {
+	if (request.query.search) {
+		const regex = new RegExp(escapeRegex(request.query.search), 'gi');
+		//Getting all the streamers from Database
+		Streamer.find({ name: regex }, (error, allStreamers) => {
+			if (error) {
+				console.log(error);
+			} else {
+				if (allStreamers.length < 1) {
+					request.flash('error', 'No streamer found from your search');
+					return response.redirect('/streamers');
+				}
+				//Renders all the streamers and also contains the information of currently loged user
+				response.render('streamers/streamers', { streamers: allStreamers });
+			}
+		});
+	} else {
+		//Getting all the streamers from Database
+		Streamer.find({}, (error, allStreamers) => {
+			if (error) {
+				console.log(error);
+			} else {
+				//Renders all the streamers and also contains the information of currently loged user
+				response.render('streamers/streamers', { streamers: allStreamers });
+			}
+		});
+	}
 });
 
 //Making a RESTful convention
-router.get("/new", middleware.isLoggedIn, (request, response) => {
-    response.render("streamers/new");
+router.get('/new', middleware.isLoggedIn, (request, response) => {
+	response.render('streamers/new');
 });
 
 //Posting Data from an input to our Streamers page
-router.post("/", middleware.isLoggedIn, (request, response) => {
-    //Get data from a form and add it to streamers array
-    const name = request.body.name;
-    const logo = request.body.logo;
-    const desc = request.body.description;
-    const author = {
-        id: request.user._id,
-        username: request.user.username
-    };
-    const newStreamer = { name: name, logo: logo, description: desc, author: author };
-    //Create a new Streamer and save it to Database
-    Streamer.create(newStreamer, (error, newlyCreatedStreamer) => {
-        if (error) {
-            console.log(error);
-        } else {
-            //Redirect back to the Streamers page
-            response.redirect("/streamers");
-        };
-    });
+router.post('/', middleware.isLoggedIn, (request, response) => {
+	//Get data from a form and add it to streamers array
+	const name = request.body.name;
+	const logo = request.body.logo;
+	const desc = request.body.description;
+	const author = {
+		id: request.user._id,
+		username: request.user.username
+	};
+	const newStreamer = { name: name, logo: logo, description: desc, author: author };
+	//Create a new Streamer and save it to Database
+	Streamer.create(newStreamer, (error, newlyCreatedStreamer) => {
+		if (error) {
+			console.log(error);
+		} else {
+			//Redirect back to the Streamers page
+			response.redirect('/streamers');
+		}
+	});
 });
 
 //Showing the Streamer individually with more info
-router.get("/:id", (request, response) => {
-    //Find the Streamer with provided ID
-    Streamer.findById(request.params.id).populate("comments").exec((error, foundStreamer) => {
-        if (error || !foundStreamer) {
-            request.flash("error", "Influencer not found!");
-            response.redirect("/streamers");
-        } else {
-            //Render show the template with that Streamer
-            response.render("streamers/show", { streamer: foundStreamer });
-        };
-    });
+router.get('/:id', (request, response) => {
+	//Find the Streamer with provided ID
+	Streamer.findById(request.params.id).populate('comments').exec((error, foundStreamer) => {
+		if (error || !foundStreamer) {
+			request.flash('error', 'Influencer not found!');
+			response.redirect('/streamers');
+		} else {
+			//Render show the template with that Streamer
+			response.render('streamers/show', { streamer: foundStreamer });
+		}
+	});
 });
 
 //Edit Streamer Route
-router.get("/:id/edit", middleware.checkStreamerOwnership, (request, response) => {
-    Streamer.findById(request.params.id, (error, foundStreamer) => {
-        response.render("streamers/edit", { streamer: foundStreamer });
-    });
+router.get('/:id/edit', middleware.checkStreamerOwnership, (request, response) => {
+	Streamer.findById(request.params.id, (error, foundStreamer) => {
+		response.render('streamers/edit', { streamer: foundStreamer });
+	});
 });
 
 //Update Streamer Route
-router.put("/:id", middleware.checkStreamerOwnership, (request, response) => {
-    //Find and update the correct Streamer
-    Streamer.findByIdAndUpdate(request.params.id, request.body.streamer, (error, updatedStreamer) => {
-        if (error) {
-            response.redirect("/streamers");
-        } else {
-            //Redirect back to the Show Page
-            response.redirect("/streamers/" + request.params.id);
-        };
-    });
+router.put('/:id', middleware.checkStreamerOwnership, (request, response) => {
+	//Find and update the correct Streamer
+	Streamer.findByIdAndUpdate(request.params.id, request.body.streamer, (error, updatedStreamer) => {
+		if (error) {
+			response.redirect('/streamers');
+		} else {
+			//Redirect back to the Show Page
+			response.redirect('/streamers/' + request.params.id);
+		}
+	});
 });
 
 //Destroy Streamer Route
-router.delete("/:id", middleware.checkStreamerOwnership, (request, response, next) => {
-    Streamer.findById(request.params.id, (error, streamer) => {
-        //Removing the comments associated with that Streamer
-        Comment.deleteMany({
-            "_id": {
-                $in: streamer.comments
-            }
-        }, (error) => {
-            if (error) return next(error);
-            request.flash("success", "Streamer deleted!");
-            streamer.remove();
-            response.redirect("/streamers");
-        });
-    });
+router.delete('/:id', middleware.checkStreamerOwnership, (request, response, next) => {
+	Streamer.findById(request.params.id, (error, streamer) => {
+		//Removing the comments associated with that Streamer
+		Comment.deleteMany(
+			{
+				_id: {
+					$in: streamer.comments
+				}
+			},
+			(error) => {
+				if (error) return next(error);
+				request.flash('success', 'Streamer deleted!');
+				streamer.remove();
+				response.redirect('/streamers');
+			}
+		);
+	});
 });
 
-
+//Making a fuzzy search
+function escapeRegex(text) {
+	return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
 //We declare that we have to export router
 module.exports = router;
